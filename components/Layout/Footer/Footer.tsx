450ab3d2ab77a98177845f78e338b82ababc14d7
Chi-Luc <chilucdiep@hotmail.com>
2022-04-06 21:20:00 -0400

    Footer setup

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,8 +1,39 @@
+import styles from "./Footer.module.scss";
+
 export function Footer() {
-    return (
-      <footer>
-        footer
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className={styles.Footer}>
+      <h2>Footer</h2>
+      <div className="links">
+        <ul>
+          <li>
+            <h2>Explore</h2>
+            <a href="/">ok</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <h2>Contact us</h2>
+            <a href="/">ok</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <h2>Follow us</h2>
+            <a href="/">ok</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <h2>Begin your adventure</h2>
+            <a href="/">ok</a>
+          </li>
+        </ul>
+      </div>
+      <div className="copy">
+        <p className="BigCaption">ok</p>
+        <p className="BigCaption">ok</p>
+      </div>
+    </footer>
+  );
+}
