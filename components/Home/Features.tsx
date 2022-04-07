d71bbcab033a08c9c4f7dbc3517e128c71b94e9d
Chi-Luc <chilucdiep@hotmail.com>
2022-04-07 09:57:38 -0400

    Features

--- components/Home/Features.tsx
+++ components/Home/Features.tsx
@@ -1,13 +1,35 @@
 import Image from "next/image";
 
+import SpeedIcon from "../../images/SpeedIcon.svg";
 import HomeIcon from "../../images/HomeIcon.svg";
+import CheckIcon from "../../images/CheckIcon.svg";
 import styles from "./Home.module.scss";
 
 export default function Features() {
   return (
-    <section>
+    <section className={styles.Features}>
       <h1>What we offer</h1>
-      <div></div>
+      <div className={styles.Grid}>
+        <GridItem
+          image={SpeedIcon}
+          title="Slingshot bahay"
+          text="Our Slingshot looks, sounds, and drives like nothing else on the road. Leave four wheels behind and make your mark on the open road.
+"
+        />
+        <GridItem
+          image={HomeIcon}
+          title="Easy renting bahay"
+          text="Our Slingshot looks, sounds, and drives like nothing else on the road. Leave four wheels behind and make your mark on the open road.
+
+"
+        />
+        <GridItem
+          image={CheckIcon}
+          title="No special license required!"
+          text="Our Slingshot looks, sounds, and drives like nothing else on the road. Leave four wheels behind and make your mark on the open road.
+"
+        />
+      </div>
     </section>
   );
 }
