a36fb7b24736122c42e02e6de7fb6ff5ed7c46d9
Chi-Luc <chilucdiep@hotmail.com>
2022-04-11 16:40:55 -0400

    Layout interface

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,12 @@
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
-import styles from './Layout.module.scss'
 
-export function Layout({ children }: any) {
+import styles from "./Layout.module.scss";
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
   return (
     <div className={styles.Container}>
       <Navbar />
