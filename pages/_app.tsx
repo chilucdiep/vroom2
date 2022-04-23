19da07c61441756c56505f62b3c2f3a8db37cd7c
Chi-Luc <chilucdiep@hotmail.com>
2022-04-23 10:03:57 -0400

    Meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
+import Head from "next/head";
+
 import { Layout } from "../components/Layout";
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://www.vroom2.ca/public/Thumbnail.png"
+        />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
