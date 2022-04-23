ebbfc71eea271a393f3f40efddc33da6089914fc
Chi-Luc <chilucdiep@hotmail.com>
2022-04-23 10:05:01 -0400

    Meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
-import Head from "next/head";
-
 import { Layout } from "../components/Layout";
 import "../styles/globals.scss";
 
