e25f2ccb74d22b6da9754ddcbff3eeb4f41dcfe0
Chi-Luc <chilucdiep@hotmail.com>
2022-06-12 19:01:23 -0400

    Booking & Pricing

--- pages/booking.tsx
+++ pages/booking.tsx
@@ -7,12 +7,47 @@ export default function AboutPage() {
         <title>Vroom 2 | Book your Slingshot!</title>
       </Head>
 
-      <div style={{ height: "68vh", display: "grid", placeContent: "center" }}>
-        <h1 className="BigTitle">Coming soon!</h1>
-        <p>
-          Call us at 438-700-9091 or contact us via email at
-          vroom2rental@gmail.com
-        </p>
+      <div
+        style={{
+          height: "90vh",
+          display: "grid",
+          placeContent: "center",
+          gap: "5em",
+          textAlign: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            placeContent: "center",
+            gap: "2em",
+            maxWidth: "750px",
+          }}
+        >
+          <h1 className="BigTitle">Book your Slingshot today!</h1>
+          <h3>
+            Call us at 438-700-9091 or contact us via email at
+            vroom2rental@gmail.com
+          </h3>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            gap: "1em",
+            textAlign: "left",
+          }}
+        >
+          <h4>Pricing:</h4>
+          <p>
+            <strong>weekday:</strong> 270$/day
+            <br />
+            <strong>weekend:</strong> 300$/day
+            <br />
+            <strong>3 days or more:</strong> 250$/day on weekdays and 270$/day
+            on weekends
+          </p>
+        </div>
+        <h3>Complete online booking system coming soon!</h3>
       </div>
     </>
   );
