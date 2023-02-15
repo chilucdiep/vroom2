b8317bd66efab0d07d6a2b940aee43cbc05ae380
chiludiep <chiludiep@hotmail.com>
2023-02-15 15:39:02 -0500

    loading

--- components/About/Hero.tsx
+++ components/About/Hero.tsx
@@ -1,14 +1,32 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 import Logo from "../../public/favicon.ico";
 import styles from "../About/About.module.scss";
 
 export default function Hero() {
+  const textVariant = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 0.09,
+      transition: {
+        delay: 0.5,
+        duration: 1.5,
+        loop: true,
+      },
+    },
+  };
+
   return (
     <section className={styles.Hero}>
-      <div className={styles.BackSplash}>
+      <motion.div
+        className={styles.BackSplash}
+        variants={textVariant}
+        initial="hidden"
+        animate="visible"
+      >
         <Image src={Logo} alt="BackSplash" />
-      </div>
+      </motion.div>
       <h3>Vroom2 isn’t just any ordinary car rental company</h3>
       <h1 className="BigTitle">
         WE ARE AN EXCLUSIVE EXPERIENCE{" "}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,12 @@ import type { AppProps } from "next/app";
 
 import { Layout } from "../components/Layout";
 import "../styles/globals.scss";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -19,9 +23,37 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          // transition={{
+          //   duration: 0.1,
+          // }}
+          variants={{
+            initialState: {
+              opacity: 0,
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            },
+            animateState: {
+              opacity: 1,
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            },
+            exitState: {
+              clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+              backgroundImage:
+                "https://raw.githubusercontent.com/chilucdiep/vroom2/main/public/Thumbnail.png",
+              transition: { duration: 0.5 },
+            },
+          }}
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
