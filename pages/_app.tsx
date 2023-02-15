36b3616b7cef2d5d5f2db57994f97de09fc655a1
chiludiep <chiludiep@hotmail.com>
2023-02-15 16:11:20 -0500

    loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,34 +23,34 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
 
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={router.route}
-          initial="initialState"
-          animate="animateState"
-          exit="exitState"
-          transition={{
-            duration: 0.5,
-          }}
-          variants={{
-            initialState: {
-              opacity: 0,
-              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-            },
-            animateState: {
-              opacity: 1,
-              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-            },
-            exitState: {
-              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-            },
-          }}
-        >
-          <Layout>
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={router.route}
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            transition={{
+              duration: 0.5,
+            }}
+            variants={{
+              initialState: {
+                opacity: 0,
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              },
+              animateState: {
+                opacity: 1,
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              },
+              exitState: {
+                clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+              },
+            }}
+          >
             <Component {...pageProps} />
-          </Layout>
-        </motion.div>
-      </AnimatePresence>
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
     </>
   );
 }
